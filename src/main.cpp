e3522f0010eb3fa4357a100d241fc95132994d1e
Lemonical <jesuislemon@gmail.com>
2025-09-08 17:20:11 +0800

    Add project files.

---
+++ src/main.cpp
@@ -0,0 +1,30 @@
+﻿#include <iostream>
+#include <glad/glad.h>
+#include <GLFW/glfw3.h>
+
+int main() {
+    glfwInit();
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
+    glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+
+    // Placeholder
+    constexpr int width = 640,
+                  height = 480;
+
+    GLFWwindow* window = glfwCreateWindow(width, height, "GrapeEngine", NULL, NULL);
+    if (window == NULL) {
+        std::cout << "Failed to create GLFW window" << '\n';
+    }
+    glfwMakeContextCurrent(window);
+    gladLoadGL();
+    glViewport(0, 0, width, height);
+
+    while (!glfwWindowShouldClose(window)) {
+        glfwPollEvents();
+    }
+
+    glfwDestroyWindow(window);
+    glfwTerminate();
+    return 0;
+}
