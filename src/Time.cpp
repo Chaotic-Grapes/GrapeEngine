adab32db8d80e18a4ad2bfc030874c8ef6fcfa56
Lemonical <jesuislemon@gmail.com>
2025-09-10 17:14:57 +0800

    Add unscaled time, frame count and maximum delta time

--- src/Time.cpp
+++ src/Time.cpp
@@ -3,10 +3,12 @@
 std::chrono::high_resolution_clock::time_point Time::StartTime;
 std::chrono::high_resolution_clock::time_point Time::LastFrameTime;
 
-float Time::Dt = 0.f;
-float Time::FixedDt = 1.0f / 60.0f;
-float Time::TimeElapsed = 0.f;
-float Time::TimeScaling = 1.f;
+float Time::Dt           = 0.f;
+float Time::FixedDt      = 1.f / 60.f;
+float Time::TimeElapsed  = 0.f;
+float Time::TimeScaling  = 1.f;
+int   Time::FrameCounter = 0;
+float Time::MaximumDt    = 0.4f;
 
 void Time::Initialize() {
     StartTime = std::chrono::high_resolution_clock::now();
@@ -18,18 +20,29 @@ void Time::Update() {
     const auto& now = std::chrono::high_resolution_clock::now();
     const std::chrono::duration<float>& frameTime = now - LastFrameTime;
     LastFrameTime = now;
-    Dt = frameTime.count();
+    Dt = std::max(frameTime.count(), MaximumDt);
     
     // Total time since start
     TimeElapsed = std::chrono::duration<float>(now - StartTime).count();
+
+    // Frame count
+    ++FrameCounter;
 }
 
 void Time::TimeScale(const float& scale) {
     TimeScaling = std::max(scale, 0.f);
 }
 
-std::string Time::Name() const  { return "Time"; }
-float Time::DeltaTime()         { return Dt; }
-float Time::FixedDeltaTime()    { return FixedDt; }
-float Time::ElapsedTime()       { return TimeElapsed; }
-float Time::TimeScale()         { return TimeScaling; }
+void Time::MaximumDeltaTime(const float& maxDelta) {
+    MaximumDt = std::max(maxDelta, 0.f);
+}
+
+std::string Time::Name() const          { return "Time"; }
+float Time::DeltaTime()                 { return Dt * TimeScaling; }
+float Time::UnscaledDeltaTime()         { return Dt; }
+float Time::FixedDeltaTime()            { return FixedDt * TimeScaling; }
+float Time::UnscaledFixedDeltaTime()    { return FixedDt; }
+float Time::ElapsedTime()               { return TimeElapsed; }
+float Time::TimeScale()                 { return TimeScaling; }
+int   Time::FrameCount()                { return FrameCounter; }
+float Time::MaximumDeltaTime()          { return MaximumDt; }
--- src/Time.h
+++ src/Time.h
@@ -18,10 +18,15 @@ public:
     std::string Name() const override;
     
     static float DeltaTime();
+    static float UnscaledDeltaTime();
     static float FixedDeltaTime();
+    static float UnscaledFixedDeltaTime();
     static float ElapsedTime();
     static void  TimeScale(const float& scale);
     static float TimeScale();
+    static int   FrameCount();
+    static void  MaximumDeltaTime(const float& maxDelta);
+    static float MaximumDeltaTime();
     
 private:
     static std::chrono::high_resolution_clock::time_point StartTime;
@@ -31,5 +36,7 @@ private:
     static float FixedDt;
     static float TimeElapsed;
     static float TimeScaling;
+    static int   FrameCounter;
+    static float MaximumDt;
 };
 #endif // TIME_H
