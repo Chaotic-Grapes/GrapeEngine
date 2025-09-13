9e304a9db2f85ce65d8d490694147d93e1aeddad
Lemonical <jesuislemon@gmail.com>
2025-09-14 00:12:02 +0800

    Use glfwGetTime() instead of chrono

--- src/Time.h
+++ src/Time.h
@@ -1,11 +1,9 @@
 ﻿#ifndef TIME_H
 #define TIME_H
 
-#include <chrono>
 #include "ISystem.h"
 
-class Time final : public Engine::ISystem
-{
+class Time final : public Engine::ISystem {
 public:
     /// Call this function before any other methods of the Time class
     void Initialize() override;
@@ -17,26 +15,26 @@ public:
     /// Debug name
     std::string Name() const override;
 
-    static float DeltaTime();
-    static float UnscaledDeltaTime();
-    static float FixedDeltaTime();
-    static float UnscaledFixedDeltaTime();
-    static float ElapsedTime();
-    static void  TimeScale(const float& scale);
-    static float TimeScale();
-    static int   FrameCount();
-    static void  MaximumDeltaTime(const float& maxDelta);
-    static float MaximumDeltaTime();
+    static float  DeltaTime();
+    static float  UnscaledDeltaTime();
+    static float  FixedDeltaTime();
+    static float  UnscaledFixedDeltaTime();
+    static double ElapsedTime();
+    static void   TimeScale(const float& scale);
+    static float  TimeScale();
+    static int    FrameCount();
+    static void   MaximumDeltaTime(const float& maxDelta);
+    static float  MaximumDeltaTime();
     
 private:
-    static std::chrono::high_resolution_clock::time_point m_startTime;
-    static std::chrono::high_resolution_clock::time_point m_lastFrameTime;
+    static double m_startTime;
+    static double m_lastFrameTime;
     
-    static float m_deltaTime;
-    static float m_fixedDeltaTime;
-    static float m_timeElapsed;
-    static float m_timeScale;
-    static int   m_frameCounter;
-    static float m_maximumDeltaTime;
+    static float  m_deltaTime;
+    static float  m_fixedDeltaTime;
+    static double m_timeElapsed;
+    static float  m_timeScale;
+    static int    m_frameCounter;
+    static float  m_maximumDeltaTime;
 };
 #endif // TIME_H
