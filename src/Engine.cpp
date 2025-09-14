4e26c570beef51db5bcdf1a992fcb3f68dfd3590
dalton <daltonkoh@gmail.com>
2025-09-14 17:46:37 +0800

    Revert "add"
    This reverts commit 565cc0089e198694a3261808c35abeb13825352e.

--- src/Engine.cpp
+++ src/Engine.cpp
@@ -4,12 +4,7 @@
 #include <glm/gtc/matrix_transform.hpp>
 #include "Time.h"
 #include <iostream>
-<<<<<<< Updated upstream
 #include "GLHelper.h"
-=======
-#include "test.h"
-#include "Physics.h" 
->>>>>>> Stashed changes
 
 namespace Engine {
     /// Global pointer to the core engine
@@ -47,12 +42,6 @@ namespace Engine {
             0.0f, (float)m_mainWindow.Height(),
             -1.0f, 1.0f);
 
-        //test code
-        PhysicsSystem* physics = new PhysicsSystem();
-        AttachSystem(physics);
-        TestScene test;
-        test.init(physics, (float)m_mainWindow.Width(), (float)m_mainWindow.Height());
-
         while (!m_mainWindow.ShouldClose()) {
             m_mainWindow.PollEvents();
             Update();
@@ -64,13 +53,9 @@ namespace Engine {
             shader.use();
             shader.setMat4("uProjection", proj);
 
-            test.update(Time::DeltaTime());
-
             m_renderer->beginFrame();
 
             // Submit primitives here
-
-            test.render(*m_renderer);
 
             m_renderer->endFrame();
 
