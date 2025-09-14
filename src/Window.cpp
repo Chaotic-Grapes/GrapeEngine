2d81c4fa04de8d5d1cc45858db0c0c4620657b55
Lemonical <jesuislemon@gmail.com>
2025-09-14 17:44:45 +0800

    Add window modes

--- src/Window.cpp
+++ src/Window.cpp
@@ -1,6 +1,12 @@
 ﻿#include "Window.h"
 #include <iostream>
 
+namespace {
+	bool HasFlag(const WindowMode::Flags a, const WindowMode::Flags b) {
+		return (a & b) != static_cast<WindowMode::Flags>(0);
+	}
+}
+
 Window::~Window() { Destroy(); }
 
 bool Window::Create(const std::string& title, const int width, const int height, GLFWmonitor* monitor, GLFWwindow* parent) {
@@ -32,6 +38,20 @@ bool Window::Create(const std::string& title, const int width, const int height,
 		return false;
 	}
 	glViewport(0, 0, width, height);
+
+	// This callback function is called when the window is resized
+	// to update the viewport and store the new width and height
+	glfwSetFramebufferSizeCallback(m_windowHandle, [](GLFWwindow* window, const int w, const int h) {
+		glViewport(0, 0, w, h);
+		if (auto* self = static_cast<Window*>(glfwGetWindowUserPointer(window))) {
+			self->m_width = w;
+			self->m_height = h;
+		}
+	});
+
+	// Store the pointer to this instance for use in callbacks
+	// because GLFW does not know context
+	glfwSetWindowUserPointer(m_windowHandle, this);
 	return true;
 }
 
@@ -43,6 +63,40 @@ void Window::Destroy() {
 	}
 }
 
+void Window::Mode(const WindowMode::Flags mode, GLFWmonitor* monitor) {
+	if (mode == m_mode) return;
+
+	// TODO: When hardware manager is done, get:
+	// All monitor pos and sizes, window pos (and size?)
+	// Calculate them to get the monitor which the window is on
+	if (!monitor) 
+		monitor = glfwGetPrimaryMonitor();
+	const GLFWvidmode* modeInfo = glfwGetVideoMode(monitor);
+
+	if (HasFlag(mode, WindowMode::Borderless)) {
+		glfwSetWindowAttrib(m_windowHandle, GLFW_DECORATED, GLFW_FALSE);
+		glfwGetWindowPos(m_windowHandle, &m_windowedX, &m_windowedY);
+		glfwGetWindowSize(m_windowHandle, &m_windowedWidth, &m_windowedHeight);
+	}
+	if (HasFlag(mode, WindowMode::Windowed)) {
+		glfwSetWindowAttrib(m_windowHandle, GLFW_DECORATED, GLFW_TRUE);
+		glfwSetWindowMonitor(m_windowHandle, nullptr, m_windowedX, m_windowedY, m_windowedWidth, m_windowedHeight, 0);
+	}
+	if (HasFlag(mode, WindowMode::Fullscreen)) {
+		glfwGetWindowPos(m_windowHandle, &m_windowedX, &m_windowedY);
+		glfwGetWindowSize(m_windowHandle, &m_windowedWidth, &m_windowedHeight);
+		glfwSetWindowMonitor(m_windowHandle, monitor, 0, 0, modeInfo->width, modeInfo->height, modeInfo->refreshRate);
+	}
+	
+	m_mode = mode;
+}
+
+void Window::Resize(const int width, const int height) {
+	glfwSetWindowSize(m_windowHandle, width, height);
+	m_width = width;
+	m_height = height;
+}
+
 GLFWwindow* Window::Handle()    const { return m_windowHandle; }
 int Window::Width()				const { return m_width; }
 int Window::Height()			const { return m_height; }
@@ -51,7 +105,14 @@ void Window::SwapBuffers()		const { glfwSwapBuffers(m_windowHandle); }
 bool Window::ShouldClose()		const { return m_windowHandle && glfwWindowShouldClose(m_windowHandle); }
 void Window::Close()			const { glfwSetWindowShouldClose(m_windowHandle, true); }
 
-bool Window::IsFocused()		const { return m_windowHandle && glfwGetWindowAttrib(m_windowHandle, GLFW_FOCUSED); }
-bool Window::IsMinimized()		const { return m_windowHandle && glfwGetWindowAttrib(m_windowHandle, GLFW_ICONIFIED); }
-bool Window::IsMaximized()		const { return m_windowHandle && glfwGetWindowAttrib(m_windowHandle, GLFW_MAXIMIZED); }
-bool Window::IsVisible()		const { return m_windowHandle && glfwGetWindowAttrib(m_windowHandle, GLFW_VISIBLE); }
+bool Window::IsFocused()				  const { return m_windowHandle && glfwGetWindowAttrib(m_windowHandle, GLFW_FOCUSED); }
+bool Window::IsMinimized()				  const { return m_windowHandle && glfwGetWindowAttrib(m_windowHandle, GLFW_ICONIFIED); }
+void Window::IsMinimized(const bool flag) const { if (m_windowHandle) glfwSetWindowAttrib(m_windowHandle, GLFW_ICONIFIED, flag); }
+bool Window::IsMaximized()				  const { return m_windowHandle && glfwGetWindowAttrib(m_windowHandle, GLFW_MAXIMIZED); }
+void Window::IsMaximized(const bool flag) const { if (m_windowHandle) glfwSetWindowAttrib(m_windowHandle, GLFW_MAXIMIZED, flag); }
+bool Window::IsVisible()				  const { return m_windowHandle && glfwGetWindowAttrib(m_windowHandle, GLFW_VISIBLE); }
+void Window::IsVisible(const bool flag)	  const { if (m_windowHandle) glfwSetWindowAttrib(m_windowHandle, GLFW_VISIBLE, flag); }
+bool Window::IsResizable()				  const { return m_windowHandle && glfwGetWindowAttrib(m_windowHandle, GLFW_RESIZABLE); }
+void Window::IsResizable(const bool flag) const { if (m_windowHandle) glfwSetWindowAttrib(m_windowHandle, GLFW_RESIZABLE, flag); }
+
+bool Window::HasMode(const WindowMode::Flags value) const { return HasFlag(value, m_mode); }
