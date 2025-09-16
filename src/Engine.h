ec3fa57d99e0904e5592fa0b9ba6e5784c58450f
Lemonical <jesuislemon@gmail.com>
2025-09-16 17:29:40 +0800

    Refactor entry point for sandboxing
    - Moved the main entry point to EngineSandbox.cpp for debug builds and removed main.cpp
    - Updated Engine to support enabling or disabling the console window via a new Run(bool consoleFlag) method
    - Adjusted project settings to use the Windows subsystem instead of Console, and added functions to manage the console window
    - Also included minor fixes in WindowManager.h to avoid macro conflicts

--- src/Engine.h
+++ src/Engine.h
@@ -25,13 +25,6 @@ public:
         void DestroySystems() const;
 
         /**
-         * @brief Returns the main window of the engine
-         * 
-         * @return Window
-		 */
-        Window MainWindow() const;
-
-        /**
 		 * @brief Ticks all attached systems and others as needed
          */
         void Update() const;
@@ -44,11 +37,17 @@ public:
 
         /**
          * @brief Starts the engine
+         *
+		 * @param consoleFlag If true, runs with console output enabled
          */
-        void Run() const;
+        void Run(bool consoleFlag);
     private:
         // Systems currently attached to the Engine
         std::vector<ISystem*> m_systems;
+
+		// Functions to enable/disable console output
+        static void _enableConsole();
+        static void _disableConsole();
 };
 
     extern Engine* CORE;
