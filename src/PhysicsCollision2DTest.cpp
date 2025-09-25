ca8d26fea98e8eb3e9206a7728bf0c1d220d74ad
dalton <daltonkoh@gmail.com>
2025-09-25 09:24:49 +0800

    p

--- src/PhysicsCollision2DTest.cpp
+++ src/PhysicsCollision2DTest.cpp
@@ -33,7 +33,7 @@ Sandbox::PhysicsCollision2DTestScene::PhysicsCollision2DTestScene(const int widt
     m_pausePhysics = false;
 
     // Disable gravity for this test (balls should fly around freely)
-    Engine::Physics2D::SetGravity(Vector2D(0.0f, -200.0f));
+    Engine::Physics2D::SetGravity(Vector2D(0.0f, -30.0f));
 }
 
 void Sandbox::PhysicsCollision2DTestScene::OnLoad() {
@@ -186,7 +186,7 @@ void Sandbox::PhysicsCollision2DTestScene::SpawnCubes_T(float dt) {
     while (m_spawnAcc >= m_spawnIntervals) {
         m_spawnAcc -= m_spawnIntervals;
         // spawn 1–2 cubes per tick for variety
-        int batch = MathHelper::Randomize<int>(1, 7, 0);
+        int batch = MathHelper::Randomize<int>(2, 7, 0);
         for (int i = 0; i < batch; ++i) SpawnCubes();
     }
 }
