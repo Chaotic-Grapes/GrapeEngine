030c19833df5b55d808600b951ea498f69ec2527
Meng Yew <89077454+CitizenCathay@users.noreply.github.com>
2025-10-26 03:06:19 +0800

    Implement lightweight config file for graphics unit conversion

---
+++ include/engine/graphics/graphicsConfig.hpp
@@ -0,0 +1,7 @@
+// graphicsConfig.hpp
+#pragma once
+
+namespace graphicsConfig {
+    inline constexpr float WORLD_TO_PIXEL = 100.0f;
+    inline constexpr float PIXEL_TO_WORLD = 1.0f / WORLD_TO_PIXEL;
+}
