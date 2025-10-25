1bb18c11b39f8dedf4466d018bfb4ddca6c88a08
Meng Yew <89077454+CitizenCathay@users.noreply.github.com>
2025-10-26 02:54:47 +0800

    Change vec2 to vec3

--- include/engine/graphics/vertex.hpp
+++ include/engine/graphics/vertex.hpp
@@ -2,7 +2,7 @@
 #include <glm/glm.hpp>
 
 struct Vertex {
-    glm::vec2 position;
+    glm::vec3 position;
     glm::vec2 texCoord;
     glm::vec4 color;
     float     texIndex; // filled by renderer when you pass a textureId
