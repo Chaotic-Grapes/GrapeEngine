e8af47b29f5330d5b8783d2117d59d32b4f3c500
Meng Yew <89077454+CitizenCathay@users.noreply.github.com>
2025-10-26 03:01:00 +0800

    Change glfw swap buffer call to window class swap buffer member call

--- src/engine/core/Application.cpp
+++ src/engine/core/Application.cpp
@@ -79,7 +79,7 @@ namespace Engine {
                     m_shouldStop = true;
                     break;
                 }
-                glfwSwapBuffers(win->Handle());
+                win->SwapBuffers();
             }
 
             const double frameDuration = m_lastFrameTime - frameStart;
