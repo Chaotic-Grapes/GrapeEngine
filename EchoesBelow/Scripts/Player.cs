6702c4d2e06bf8e2f9cd56cfcdb4f2d10965ea0d
ubaidubai <ubaidubai.art@gmail.com>
2025-11-25 10:48:38 +0800

    Swimming_V1
    +Player.cs experimentation
    >Created AddTorque, AddForce functions that work similarly to Unity counterparts
    >Added static public access references to Player information like player direction (playerDir) and player position (playerPos) accessible thru Player.etc etc
    +Changes to TestScene 2 (soon to be renamed!)

--- EchoesBelow/Scripts/Player.cs
+++ EchoesBelow/Scripts/Player.cs
@@ -1,63 +1,101 @@
 using GrapeEngine.Scripting;
 using GrapeEngine.Numerics;
 using GrapeEngine.Math;
+using GrapeEngine.Physics;
 
 namespace Scripts;
 
 public class Player : ScriptBehaviour
 {
     //Constant Parameters
-    const float moveSpeed = 2f;
-    const float torqueMax = 3f;
-    const float torqueSpeed = 1f;
+    const float moveSpeed = 1f;
+    const float maxSpeed = 50f;
+    const float rotateSpeed = 70f;
+    
 
     //For Testing Purposes
-    float torqueInitial = 0f;
-
-
-
-    //Test out alternative to serializable fields!
-    public static Vector3 playerPos;
     
+    
+
+    //These are publicly available fields
+    public static Vector3 playerPos;
+    public static Vector2 playerDir;
+
+    //AngularVelocity2D av;
     public override void OnStart()
     {
         // Called once when the script is initialized
+
+        //av = GetComponent<AngularVelocity2D>();
+        //try another day
     }
     public override void OnUpdate()
     {
         //Declare and initialize components
         ref LocalTransform transform = ref GetComponent<LocalTransform>();
-        ref AngularVelocity2D torque = ref GetComponent<AngularVelocity2D>();
-
-
+        ref LinearVelocity2D linearVelocity = ref GetComponent<LinearVelocity2D>();
+        ref AngularVelocity2D angularVelocity = ref GetComponent<AngularVelocity2D>();
+        
+        
         if (Input.IsKeyDown(KeyCode.W))
         {   //Up
-            transform.Position.Y += moveSpeed * Time.DeltaTime;
+            //linearVelocity.Value = playerDir * moveSpeed * Time.DeltaTime;
+            AddForce(ref linearVelocity);
         }
         if (Input.IsKeyDown(KeyCode.S))
         {   //Down
-            transform.Position.Y -= moveSpeed * Time.DeltaTime;
+            
         }   
         if (Input.IsKeyDown(KeyCode.D))
-        {   //Right
-            //torqueInitial = Torque(torqueInitial);
-            transform.Position.X += moveSpeed * Time.DeltaTime;
+        {   //Right Key
+            AddTorque(ref angularVelocity, -1); //positive rotate direction
         }
         if (Input.IsKeyDown(KeyCode.A))
-        {   //Left
-            transform.Position.X -= moveSpeed * Time.DeltaTime;
+        {   //Left Key
+            AddTorque(ref angularVelocity, 1);//negative rotate direction
         }
 
         //store player position for other scripts to find
         playerPos = transform.Position;
 
-        //Logs
-        Log("torqueInitial: " + torqueInitial);
+        //Convert from ZYX Quaternion to angle in radians
+        float angle = Quat2EulerAxisZ(transform.Rotation);
+        //set player direction
+        playerDir = new Vector2(GMath.Cos(angle + (90 * GMath.Deg2Rad)), GMath.Cos(angle));
+
+        
+        //Log("playerDir: " + playerDir + " angle: " + angle);
     }
-    public float Torque(float currentTorque)
+    public float AddTorque(ref AngularVelocity2D angularVelocity, float input)
     {
-        currentTorque = GMath.Lerp(currentTorque, torqueMax, torqueSpeed * Time.DeltaTime);
-        return 0f;
+        //Clamp the xInput between -1 and 1
+        input = GMath.Clamp(input, -1, 1f);
+
+        //reference the angular velocity property
+        //ref AngularVelocity2D angularVelocity = ref GetComponent<AngularVelocity2D>();
+
+        angularVelocity.Value = GMath.Lerp(angularVelocity.Value, input * rotateSpeed * Time.DeltaTime, 0.2f);
+        return angularVelocity.Value;
+    }
+    private void AddForce(ref LinearVelocity2D linearVelocity)
+    { //add an impulse force
+        linearVelocity.Value += playerDir * moveSpeed * Time.DeltaTime;
+        linearVelocity.Value.X = GMath.Clamp(linearVelocity.Value.X, -maxSpeed, maxSpeed);
+        linearVelocity.Value.Y = GMath.Clamp(linearVelocity.Value.Y, -maxSpeed, maxSpeed);
+    }
+    private float Quat2EulerAxisZ(Quaternion quat)
+    {
+        //To find out how
+        //Search up Conversion of ZYX Quaternion to Euler Angle (z-yaw)
+        float x = quat.X;
+        float y = quat.Y;
+        float z = quat.Z;
+        float w = quat.W;
+
+        float a = 2 * (w * z + x * y);
+        float b = 1 - (2 * ((y * y) + (z * z)));
+        float outAngle = GMath.Atan2(a, b);
+        return outAngle;
     }
 }
 
