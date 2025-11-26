f87c92d9ac855370b79890e1cb1c737594e62f59
Lemonical <jesuislemon@gmail.com>
2025-11-26 22:49:58 +0800

    Add updated UXGD files
    Co-Authored-By: HKaiRu <102660345+HKaiRu@users.noreply.github.com>
    Co-Authored-By: ubaidubai <161317726+ubaidubai@users.noreply.github.com>

---
+++ EchoesBelow/Scripts/CollisionTestUbai.cs
@@ -0,0 +1,21 @@
+using GrapeEngine.Scripting;
+
+namespace Scripts;
+
+public class CollisionTestUbai : ScriptBehaviour
+{
+    public override void OnStart()
+    {
+        // Called once when the script is initialized
+    }
+
+    public override void OnUpdate()
+    {
+        // Called every frame
+    }
+    //public override void OnCollisionEnter()
+    //{
+
+    //}
+}
+
