4f09514de90383384a7a60cd4d74387810b4fd87
HKaiRu <hooikairu2101130@gmail.com>
2025-11-27 15:05:19 +0800

    All Assets

---
+++ EchoesBelow/Scripts/MarineSnowSpawner.cs
@@ -0,0 +1,17 @@
+using GrapeEngine.Scripting;
+
+namespace Scripts;
+
+public class MarineSnowSpawner : ScriptBehaviour
+{
+    public override void OnStart()
+    {
+        // Called once when the script is initialized
+    }
+
+    public override void OnUpdate()
+    {
+        // Called every frame
+    }
+}
+
