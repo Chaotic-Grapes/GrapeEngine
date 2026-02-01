dff4f5d17923f05f89482949843ca4e2dca9cf86
ubaidubai <ubaidubai.art@gmail.com>
2026-02-01 22:22:47 +0800

    ProcessMove V4 - Stable Rotation Interpolation ( Shortest Angular Path selected )
    Thanks to some quirky angle retrieval, A and D are swapped to facilitate the correct rotation

--- EchoesBelow/Scripts/Player.cs
+++ EchoesBelow/Scripts/Player.cs
@@ -17,7 +17,7 @@ namespace EchoesBelow.Scripts;
 [System(SystemGroup.Update, SystemRunMode.PlayOnly)]
 public class Player : SystemBase
 {
-    public static Compass abs_InputDirection = Compass.N, abs_SpriteDirection = Compass.N; //default
+    public static Compass abs_InputDirection = Compass.N;
     protected override void OnCreate()
     {
         Log("System Player initialized");
@@ -27,7 +27,7 @@ public class Player : SystemBase
     {
         foreach(var gameObject in World!.Query<PlayerComponent, LinearVelocity2D, AngularVelocity2D, LocalTransform>())
         {
-            
+            //Variables
             ref LocalTransform transform = ref gameObject.Component4;
             ref LinearVelocity2D lv = ref gameObject.Component2;
             ref AngularVelocity2D av = ref gameObject.Component3;
@@ -42,104 +42,84 @@ public class Player : SystemBase
             moveDir = ProcessInput(moveDir, lerpFac);
 
             //NaN protection for normalization
-            if (-0.0001f <= moveDir.X && moveDir.X <= 0.0001f 
-            && -0.0001f <= moveDir.Y && moveDir.Y <= 0.0001f)
-            moveDirNormalized = Vector2.Zero;
+            if (-0.0001f <= moveDir.X && moveDir.X <= 0.0001f && -0.0001f <= moveDir.Y && moveDir.Y <= 0.0001f) moveDirNormalized = Vector2.Zero;
             else moveDirNormalized = moveDir.Normalized;
 
             //Handling Rotation! Aligning Grain to moveDir=================================================
             //Convert from ZYX Quaternion to angle in radians
             //Find the local "Up" Vector of the Player. Think of this as gameObject.transform.up in Unity
-            float angle = Quat2EulerAxisZ(transform.Rotation);
-            playerDir = new Vector2(GMath.Cos(angle + (90 * GMath.Deg2Rad)), GMath.Cos(angle));
+            float playerAngle = Quat2EulerAxisZ(transform.Rotation);
+            playerDir = new Vector2(GMath.Cos(playerAngle + (90 * GMath.Deg2Rad)), GMath.Cos(playerAngle));
 
             //Find change in angle required using dot product between moveDirNormalized and playerDir
             //NaN protection when player is facing up or at rest
-            if (-0.0001f < playerDir.X && playerDir.X < 0.0001f 
-            && 0.9999f < playerDir.Y && playerDir.Y < 1.0001f)
-            playerDir = new Vector2(0,1);
+            if (-0.0001f < playerDir.X && playerDir.X < 0.0001f && 0.9999f < playerDir.Y && playerDir.Y < 1.0001f) playerDir = new Vector2(0, 1);
 
             //============================================================================================
-            //Find InputAbsDirection direction
-            if      (Input.IsKeyDown(KeyCode.W) && Input.IsKeyDown(KeyCode.A))  abs_InputDirection = Compass.NW;
-            else if (Input.IsKeyDown(KeyCode.S) && Input.IsKeyDown(KeyCode.A))  abs_InputDirection = Compass.SW;
-            else if (Input.IsKeyDown(KeyCode.S) && Input.IsKeyDown(KeyCode.D))  abs_InputDirection = Compass.SE;
-            else if (Input.IsKeyDown(KeyCode.W) && Input.IsKeyDown(KeyCode.D))  abs_InputDirection = Compass.NE;
-            else if (Input.IsKeyDown(KeyCode.W))    abs_InputDirection = Compass.N;
-            else if (Input.IsKeyDown(KeyCode.A))    abs_InputDirection = Compass.W;
-            else if (Input.IsKeyDown(KeyCode.S))    abs_InputDirection = Compass.S;
-            else if (Input.IsKeyDown(KeyCode.D))    abs_InputDirection = Compass.E;
-
-            //Find CompassDirection base on transform Rotation,  
-            //maybe next time use radians in nice increments of 0.125
-            float rotZ = Quat2EulerAxisZ(transform.Rotation) * GMath.Rad2Deg;
-            if      ( 22.5f  <rotZ&&rotZ<   67.5f)  abs_SpriteDirection = Compass.NW;
-            else if ( 112.5f <rotZ&&rotZ<  157.5f)  abs_SpriteDirection = Compass.SW;
-            else if (-157.5f <rotZ&&rotZ< -112.5f)  abs_SpriteDirection = Compass.SE;
-            else if (-67.5f  <rotZ&&rotZ< - 22.5f)  abs_SpriteDirection = Compass.NE;
-            else if (-22.5f  <rotZ&&rotZ<   22.5f)  abs_SpriteDirection = Compass.N;
-            else if ( 67.5f  <rotZ&&rotZ<  112.5f)  abs_SpriteDirection = Compass.W;
-            else if ( 157.5f <rotZ||rotZ< -157.5f)  abs_SpriteDirection = Compass.S;
-            else if (-112.5f <rotZ&&rotZ< - 67.5f)  abs_SpriteDirection = Compass.E;
-           
-            Log($"InputDirection:  {abs_InputDirection}");
-            Log($"SpriteDirection: {abs_SpriteDirection}");
-
+            RotationPolarityHandler(transform);
+            float flipFac = GMath.Clamp(HeadingDifference(playerAngle * GMath.Rad2Deg, (float)abs_InputDirection) * GMath.Rad2Deg, -1,1);
+            
             //Dot product operation to determine theta as presented by angleBetween in radians!
             float angleBetween_rad = GMath.Acos(GMath.Dot(playerDir, moveDirNormalized) / (playerDir.Magnitude * moveDirNormalized.Magnitude));
-            angleBetween_rad = (float.IsNaN(angleBetween_rad))? 0 : angleBetween_rad;
-
-            //Adjust the angles and rotduration here
-
-            //try heuristics idea
-
-
-            //float rawDifference_rad = (abs_InputDirection - abs_SpriteDirection) * GMath.Deg2Rad;
-            //Log("rawDifference: " + angleDifference);
-            //if (angleDifference < 0)
-            //{
-            //    //angleBetween_rad = 2 - angleBetween_rad;
-            //    angularVelocity = -gameObject.Component1.angularVelocity * 0.01f;
-            //    Log("Switching Polarity! of angularVelocity");
-            //}
-            //else
-            //{
-            //    angularVelocity = gameObject.Component1.angularVelocity * 0.01f;
-            //}
-            
+            angleBetween_rad = (float.IsNaN(angleBetween_rad)) ? 0 : angleBetween_rad;
 
             //Find change in time required to complete a rotation. This formula requires radians
-            float rotDuration = angleBetween_rad / angularVelocity;
-
+            //Must always be positive so we use Magnitude thru Abs
+            float rotDuration = GMath.Abs(angleBetween_rad / angularVelocity);
+            
             //start Rotation process
             bool isRotating = false;
-            if (angleBetween_rad != 0) { isRotating = true; } //Log("Start Rotating"); }
+            if (angleBetween_rad != 0) 
+            { 
+                isRotating = true; 
+            } 
             if (isRotating)
             {
                 gameObject.Component1.timer += Time.DeltaTime;
-                av.Value = GMath.Lerp(av.Value,angularVelocity,lerpFac);
+                if(flipFac >= 0) av.Value = GMath.Lerp(av.Value, angularVelocity, lerpFac);
+                else av.Value = GMath.Lerp(av.Value, -angularVelocity, lerpFac);
+               
                 //Log("Rotating. . . ");
             }
-            if(gameObject.Component1.timer > rotDuration)
+            if (gameObject.Component1.timer > rotDuration)
             {
                 isRotating = false;
                 gameObject.Component1.timer = 0;
                 av.Value = 0;
-                //Log("Finished Rotating");
             }
-            //Log($"Angle In Between: {angleBetween * GMath.Rad2Deg}");
-
-            //Assignment of linear Velocities================================================================
-            if(Input.IsKeyDown(KeyCode.W)|| Input.IsKeyDown(KeyCode.S)
-                || Input.IsKeyDown(KeyCode.A) || Input.IsKeyDown(KeyCode.D))
-            {
-                lv.Value.X += playerDir.X * moveSpeed;
-                lv.Value.Y += playerDir.Y * moveSpeed;
-                //Clamping these values to a maxSpeed
-                lv.Value.X = GMath.Clamp(lv.Value.X, -maxSpeed, maxSpeed);
-                lv.Value.Y = GMath.Clamp(lv.Value.Y, -maxSpeed, maxSpeed);
-            }
+            //Log($"playerAngle: {playerAngle}");
+            //Log($"absTarget: {abs_InputDirection}");
+            //Log("flipFac2: " + flipFac);
+            AddRelativeForce(ref lv, playerDir, moveSpeed, maxSpeed);
         }
+    }
+
+    private static void AddRelativeForce(ref LinearVelocity2D lv, Vector2 playerDir, float moveSpeed, float maxSpeed)
+    {
+        //Assignment of linear Velocities================================================================
+        if (Input.IsKeyDown(KeyCode.W) || Input.IsKeyDown(KeyCode.S)
+            || Input.IsKeyDown(KeyCode.A) || Input.IsKeyDown(KeyCode.D))
+        {
+            lv.Value.X += playerDir.X * moveSpeed;
+            lv.Value.Y += playerDir.Y * moveSpeed;
+            //Clamping these values to a maxSpeed
+            lv.Value.X = GMath.Clamp(lv.Value.X, -maxSpeed, maxSpeed);
+            lv.Value.Y = GMath.Clamp(lv.Value.Y, -maxSpeed, maxSpeed);
+        }
+    }
+
+    private void RotationPolarityHandler(LocalTransform transform)
+    {
+        //Find InputAbsDirection direction
+        //For a quirk in the angles, Keycodes D and A (left and right) are swapped!
+        if (Input.IsKeyDown(KeyCode.W) && Input.IsKeyDown(KeyCode.D)) abs_InputDirection = Compass.NW;
+        else if (Input.IsKeyDown(KeyCode.S) && Input.IsKeyDown(KeyCode.D)) abs_InputDirection = Compass.SW;
+        else if (Input.IsKeyDown(KeyCode.S) && Input.IsKeyDown(KeyCode.A)) abs_InputDirection = Compass.SE;
+        else if (Input.IsKeyDown(KeyCode.W) && Input.IsKeyDown(KeyCode.A)) abs_InputDirection = Compass.NE;
+        else if (Input.IsKeyDown(KeyCode.W)) abs_InputDirection = Compass.N;
+        else if (Input.IsKeyDown(KeyCode.D)) abs_InputDirection = Compass.W;
+        else if (Input.IsKeyDown(KeyCode.S)) abs_InputDirection = Compass.S;
+        else if (Input.IsKeyDown(KeyCode.A)) abs_InputDirection = Compass.E;
     }
 
     private static Vector2 ProcessInput(Vector2 moveDir, float lerpFac)
@@ -148,7 +128,7 @@ public class Player : SystemBase
         if (Input.IsKeyDown(KeyCode.S)) moveDir.Y = GMath.Lerp(moveDir.Y, -1, lerpFac);
         if (Input.IsKeyDown(KeyCode.A)) moveDir.X = GMath.Lerp(moveDir.X, -1, lerpFac);
         if (Input.IsKeyDown(KeyCode.D)) moveDir.X = GMath.Lerp(moveDir.X, 1, lerpFac);
-
+        //Always decelerrating moveDir but at a slower rate
         moveDir.X = GMath.Lerp(moveDir.X, 0, lerpFac / 2);
         moveDir.Y = GMath.Lerp(moveDir.Y, 0, lerpFac / 2);
         return moveDir;
@@ -168,7 +148,7 @@ public class Player : SystemBase
         float outAngle = GMath.Atan2(a, b);
         return outAngle;
     }
-    private float AngleDifference(float heading1, float heading2)
+    private float HeadingDifference(float heading1, float heading2)
     {
         float diff = (heading2 - heading1 + 180) % 360 - 180;
         return diff < -180 ? diff + 360 : diff;
