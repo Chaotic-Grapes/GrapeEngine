a7871a49b358aa2f166241ce8fefd3d4364fea81
ubaidubai <ubaidubai.art@gmail.com>
2026-02-06 11:34:05 +0800

    Pause Menu can only be called in Levels and not Start Screen

--- EchoesBelow/Scripts/Menu/PauseMenu.cs
+++ EchoesBelow/Scripts/Menu/PauseMenu.cs
@@ -1,34 +1,46 @@
+using EchoesBelow.Scripts;
 using GrapeEngine.Math;
 using GrapeEngine.Scripting.Components;
+using GrapeEngine.Scripting.Core;
+using GrapeEngine.Scripting.Events;
 using GrapeEngine.Scripting.Services;
 using GrapeEngine.Scripting.Systems;
 using GrapeEngine.Scripting.Systems.Attributes;
-using GrapeEngine.Scripting.Core;
-using GrapeEngine.Scripting.Events;
 using System.Collections.Generic;
 
 namespace Scripts.Menu;
 
-
+[Component] public record struct PauseMenuComponent(bool isPauseable, int resumeSiginifier, int exitSignifier, bool start);
 [System(SystemGroup.Update, SystemRunMode.PlayOnly)]
 public class PauseMenu : SystemBase
 {
     private const string SourceSceneName = "Level_One";
     private const string TargetScenePath = "EchoesBelow/Scenes/M4StartScene.scn";
     bool isPaused = false;
+
+    public static List<ulong> pauseMenuElementObjIds;
     protected override void OnCreate()
     {
         Log("System PauseMenu initialized");
     }
+    private bool OnStart(ref bool startBool)
+    {
+        if (startBool == true) return true;
+        startBool = true;
+        //Todo
+        pauseMenuElementObjIds = new List<ulong>();
 
+        //End of Start
+        return true;
+    }
     protected override void OnUpdate()
     {
-        SceneManager sceneManager = SceneManager.Instance;
-        Scene? active = sceneManager.GetActive();
-        if (active == null || !string.Equals(active.Name, SourceSceneName, StringComparison.Ordinal))
+        foreach(var pauseController in World!.Query<PauseMenuComponent>())
         {
-            Log($"PauseMenu: Active scene is not the source scene: '{active?.Name ?? "null"}'; aborting switch.");
-            return;
+            bool start = pauseController.Component1.start;
+            pauseController.Component1.start = OnStart(ref start);
+
+            if (!pauseController.Component1.isPauseable) return;
         }
 
         if (!isPaused && Input.IsKeyPressed(KeyCode.Escape))
@@ -45,6 +57,31 @@ public class PauseMenu : SystemBase
             //Close Pause Menu
             Log(() => "unPaused");
         }
+
+        
     }
 
+}
+[Component] public record struct AddToPauseMenuListComponent(bool start);
+[System(SystemGroup.Update, SystemRunMode.PlayOnly)]
+public class AddToPauseMenuList : SystemBase
+{
+    private bool OnStart(ref bool startBool, ulong pauseMenuElementId)
+    {
+        if (startBool == true) return true;
+        startBool = true;
+        //Todo
+        PauseMenu.pauseMenuElementObjIds.Add(pauseMenuElementId);
+
+        //End of Start
+        return true;
+    }
+    protected override void OnUpdate()
+    {
+        foreach(var pauseMenuElement in World!.Query<AddToPauseMenuListComponent>())
+        {
+            bool start = pauseMenuElement.Component1.start;
+            pauseMenuElement.Component1.start = OnStart(ref start, pauseMenuElement.Entity.Id);
+        }
+    }
 }
